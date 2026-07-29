8c11aab48e27cf24860696fa01f0887e4d430375
Ritika Molawade <ritikamolawade736@gmail.com>
2026-07-29 23:17:53 +0530

    First commit

---
+++ for.py
@@ -0,0 +1,74 @@
+# n=int(input("Enter the numbers:"))
+
+# for i in range(1,n+1):
+#     print(i)
+    
+##print a even no. upto n
+
+# n=int(input("Enter a number"))
+
+# for i in range(2,n+2,2):
+
+#         print(i)
+
+## write a program to print odd no. upto n
+
+# n=int(input("Enter a number:"))
+# for i in range(1,n+1,2):
+#     print(i)
+
+##write a program to print squares of the no.
+
+# n=int(input("Enter a number:"))
+# for i in range(1,n+1):
+#    square=n**2
+#    print(square)
+
+# n=int(input("Enter a number:"))
+# if n<=1:
+#     print("n is not a prime no.")
+# else:
+#     for i in range(2,n):
+#         if(n%2==0):
+#             print("number is not prime")
+#             break
+#         else:
+#             print("number is a prime number")
+
+# for i in range(0,3):
+# #     print("ABC")
+
+# s="ABCDE"
+# for i in range(1,6):
+#     print(s[:i])
+
+# n = int(input("Enter a number: "))
+
+# for i in range(n, 0, -1):
+#     for j in range(i):
+#         print(chr(65 + j), end="")
+#     print()
+
+# n = int(input("Enter a number: "))
+
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
+
+
+# n = int(input("Enter a number: "))
+
+# for i in range(1, n + 1):
+#     for j in range(i):
+#         print(i, end="")
+#     print()
+
+
+# n=int(input("Enter a number:"))
+
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(i,end="")
+#         print()
+
---
+++ while.py
@@ -0,0 +1,31 @@
+# n=int(input("Enter a number:"))
+# i=1
+
+# while i<=n:
+#     print(i)
+#     i=i+1
+
+##2 print a even no.upto n
+# n=int(input("Enter a number:"))
+# i=2 
+
+# while i <=n:
+#     print(i)
+#     i=i+2
+
+##3 print a odd no.upto n
+# n=int(input("Enter a number:"))
+# i=1
+
+# while i<=n:
+#     print(i)
+#     i=i+2
+
+## print sum of n natural numbers
+n=int(input("Enter a number:"))
+i=1
+sum=0
+while i<=n:
+    sum=sum+i
+    i=i+1
+print("sum=",sum)
