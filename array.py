9b8be0158a1f517974f682d1dafccc0a31079ee4
Ritika Molawade <ritikamolawade736@gmail.com>
2026-08-20 12:54:25 +0530

    Array operation

---
+++ array.py
@@ -0,0 +1,19 @@
+arr = [10, 20, 30, 40, 50]
+
+# Add element
+arr.append(60)
+print("After adding:", arr)
+
+# Remove element
+arr.remove(30)
+print("After removing:", arr)
+
+# Access element
+print("First element:", arr[0])
+
+# Find length
+print("Length:", len(arr))
+
+# Reverse array
+arr.reverse()
+print("Reversed:", arr)
