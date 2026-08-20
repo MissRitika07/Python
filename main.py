f551140e5d32dde51e464edb13ccb224ddd3c103
Ritika Molawade <ritikamolawade736@gmail.com>
2026-08-20 12:43:52 +0530

    Modules

---
+++ main.py
@@ -0,0 +1,6 @@
+from mypackage.calculation import add, multiply
+from mypackage.message import welcome
+
+print(welcome("Ritika"))
+print("Addition:", add(10, 5))
+print("Multiplication:", multiply(10, 5))
